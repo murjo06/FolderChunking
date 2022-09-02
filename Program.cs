3862387686237166af8559d1187d59f3a5a8660b
murjo06 <markvlogs228@gmail.com>
2022-09-02 21:58:32 +0200

    Some more changes, nothin much

--- Program.cs
+++ Program.cs
@@ -4,5 +4,27 @@ using System.IO;
 internal class Program {
     public static void Main(string[] args) {
         Chunker chunker = new Chunker(@"/Users/markmarjanovic/Desktop/Files/FolderChunking", @"/Users/markmarjanovic/Desktop/Files/test");
+        Console.Write("Please enter the source location: ");
+        string source = Console.ReadLine();
+        Console.Write("Please enter the target location: ");
+        string target = Console.ReadLine();
+        Console.Write("Would you live to save your location preferences? y/n  ");
+        string saveString = Console.ReadLine();
+        while(true) {
+            if(saveString != "y" && saveString != "n") {
+                Console.Write("Would you live to save your location preferences? y/n  ");
+                saveString = Console.ReadLine();
+            }
+        }
+        bool save = saveString == "y";
+        string savePreference;
+        if(save) {
+            Console.Write("\n1 - save source, 2 - save target, 3 - save both  ");
+            savePreference = Console.ReadLine();
+            if(savePreference != "1" && savePreference != "2" && savePreference != "3") {
+                Console.Write("\n1 - save source, 2 - save target, 3 - save both  ");
+                savePreference = Console.ReadLine();
+            }
+        }
     }
 }
