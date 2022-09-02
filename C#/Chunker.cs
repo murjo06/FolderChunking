62db4e83084dfcb8bc49c7e8d53051bd2394b267
murjo06 <markvlogs228@gmail.com>
2022-09-02 19:32:49 +0200

    Update Chunker.cs

--- C#/Chunker.cs
+++ C#/Chunker.cs
@@ -3,14 +3,33 @@ namespace System.IO {
     public class Chunker {
         public string from;
         public string to;
+        public Tree tree;
     
         public Chunker(string from, string to) {
             from = from;
             to = to;
         }
-    
+
+        public void GenerateTree() {
+            tree = new Tree();
+        }
         public void StartChonk() {
         
+        }
+        public void TransferFiles(DirectoryInfo source, DirectoryInfo target) {
+            if (source.FullName.ToLower() == target.FullName.ToLower()) {
+                return;
+            }
+            if (Directory.Exists(target.FullName) == false) {
+                Directory.CreateDirectory(target.FullName);
+            }
+            foreach (FileInfo file in source.GetFiles()) {
+                file.CopyTo(Path.Combine(target.ToString(), fi.Name), true);
+            }
+            foreach (DirectoryInfo directorySourceSub in source.GetDirectories()) {
+                DirectoryInfo nextTargetSubDir = target.CreateSubdirectory(directorySourceSub.Name);
+                TransferFiles(directorySourceSub, nextTargetSubDir);
+            }
         }
     
         public static long DirectorySize(DirectoryInfo directory) {
