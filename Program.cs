a9db98c61ebae0b12effc1c2a648a2ea208c843b
murjo06 <markvlogs228@gmail.com>
2022-09-02 22:35:05 +0200

    Added save files

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using System.IO;
 
 internal class Program {
     public static void Main(string[] args) {
+
+        // TODO : add save file reading
+
         Chunker chunker = new Chunker(@"/Users/markmarjanovic/Desktop/Files/FolderChunking", @"/Users/markmarjanovic/Desktop/Files/test");
         Console.Write("Please enter the source location: ");
         string source = Console.ReadLine();
@@ -14,6 +17,8 @@ internal class Program {
             if(saveString != "y" && saveString != "n") {
                 Console.Write("Would you live to save your location preferences? y/n  ");
                 saveString = Console.ReadLine();
+            } else {
+                break;
             }
         }
         bool save = saveString == "y";
@@ -21,9 +26,20 @@ internal class Program {
         if(save) {
             Console.Write("\n1 - save source, 2 - save target, 3 - save both  ");
             savePreference = Console.ReadLine();
-            if(savePreference != "1" && savePreference != "2" && savePreference != "3") {
-                Console.Write("\n1 - save source, 2 - save target, 3 - save both  ");
-                savePreference = Console.ReadLine();
+            while(true) {
+                if(savePreference != "1" && savePreference != "2" && savePreference != "3") {
+                    Console.Write("\n1 - save source, 2 - save target, 3 - save both  ");
+                    savePreference = Console.ReadLine();
+                } else {
+                    break;
+                }
+            }
+            if(savePreference == "1") {
+                File.WriteAllText("paths.txt", source);
+            } else if(savePreference == "2") {
+                File.WriteAllText("paths.txt", $"&{target}");
+            } else if(savePreference == "3") {
+                File.WriteAllText("paths.txt", $"{source}&{target}");
             }
         }
     }
