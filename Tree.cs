da73fbec017975478fae721d607c871e9959c831
murjo06 <markvlogs228@gmail.com>
2022-09-02 16:19:07 +0200

    Update Tree.cs

--- Tree.cs
+++ Tree.cs
@@ -1,13 +1,11 @@
 namespace System {
     public struct Tree {
         public TreeNode[] nodes;
-        public int[] numericNodes;
 
         public Tree(int[] values, Nullable<int> parent) {
             var i = 0;
             foreach(int value in values) {
                 nodes[i] = new TreeNode(value, this);
-                numericNodes[i] = i;
                 i++;
             }
         }
