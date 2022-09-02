fbba119bcd8d6dc09ec32698ae3f9e58dbd1dfc4
murjo06 <markvlogs228@gmail.com>
2022-09-02 17:32:29 +0200

    Update Tree.cs

--- C#/Tree.cs
+++ C#/Tree.cs
@@ -1,9 +1,12 @@
 namespace System {
     public struct Tree {
         public TreeNode[] nodes;
+        public int Length {get {return nodes.Length;}}
 
-        public Tree(dynamic[] values, Nullable<int> parent) {
+        public Tree(dynamic[] values, Nullable<int>[] parent) {
+            nodes = new TreeNode[values.Length];
             var i = 0;
+            var currentParent;
             foreach(int value in values) {
                 nodes[i] = new TreeNode(value, this);
                 i++;
