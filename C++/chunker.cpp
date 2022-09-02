6617928e2d2ce49d1f9bb1c571199f2c01b1d768
murjo06 <markvlogs228@gmail.com>
2022-09-02 17:20:26 +0200

    C++ updates

---
+++ C++/chunker.cpp
@@ -0,0 +1,15 @@
+#include <string>
+class chunker {
+    public:
+        string from;
+        string to;
+    
+        chunker(string from, string to) {
+            from = from;
+            to = to;
+        }
+    
+        int StartChonk() {
+            return 0;
+        }
+};
