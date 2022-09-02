52933a874c07206ee38488d47edde6722f444e80
murjo06 <markvlogs228@gmail.com>
2022-09-02 22:35:34 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ internal class Program {
     public static void Main(string[] args) {
 
         // TODO : add save file reading
+        // TODO : add save file overriding without data loss
 
         Chunker chunker = new Chunker(@"/Users/markmarjanovic/Desktop/Files/FolderChunking", @"/Users/markmarjanovic/Desktop/Files/test");
         Console.Write("Please enter the source location: ");
