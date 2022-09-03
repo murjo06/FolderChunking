be5093ccffeb71681571dddfe169fb9be9c903ac
murjo06 <markvlogs228@gmail.com>
2022-09-03 12:31:18 +0200

    Update TreeNode.cs

--- TreeNode.cs
+++ TreeNode.cs
@@ -1,17 +1,14 @@
 namespace System {
     public struct TreeNode {
         public object value;
-        public Tree tree;
         public Nullable<int> parent;
 
-        public TreeNode(object value, Tree tree) {
+        public TreeNode(object value) {
             this.value = value;
-            this.tree = tree;
             parent = null;
         }
-        public TreeNode(object value, Tree tree, Nullable<int> parent) {
+        public TreeNode(object value, Nullable<int> parent) {
             this.value = value;
-            this.tree = tree;
             this.parent = parent;
         }
     }
