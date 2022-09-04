f7c8908bfe6884558453b3b5bb958775f7d00fc5
murjo06 <markvlogs228@gmail.com>
2022-09-04 20:02:42 +0200

    Made some more changes :|

--- C++ [beta]/chunker.h
+++ C++ [beta]/chunker.h
@@ -12,50 +12,69 @@ class chunker {
         string from;
         string to;
     
-        chunker(string from, string to) {
-            from = from;
-            to = to;
+        chunker(string From, string To) {
+            from = From;
+            to = To;
+            if(from[lenght(from) - 1] != '/') {
+                from += '/';
+            }
+            if(to[lenght(to) - 1] != '/') {
+                to += '/';
+            }
         }
 
         void generateTree() {
-            recursive_directory_iterator iter(from);
-            recursive_directory_iterator end;
-            size_t tempArrayLenght = 0;
             struct dirent* dirent;
             struct stat dst;
             DIR* directory;
             directory = opendir(from.c_str());
+            int arrayLenght = 0;
             if(directory != NULL) {
                 for(dirent = readdir(directory); dirent != NULL; dirent = readdir(directory)) {
-                    string type = dirent->d_name;
-                    type = from + type;
-                    if(stat(type.c_str(), &dst)) {
-                        if (dst.st_mode & S_IFDIR) {
-				        	type = "folder";
-				        } else if (dst.st_mode & S_IFREG) {
-				        	type = "file";
-				        }
-                    }
+                    arrayLenght++;
                 }
                 closedir(directory);
             } else {
                 cerr << "Error detected!" << endl;
             }
-            size_t arrayLenght = tempArrayLenght;
+            struct dirent* d;
+            struct stat dt;
+            DIR* dir;
+            dir = opendir(from.c_str());
+            array<string, sizeof(arrayLenght)> names;
+            if(dir != NULL) {
+                int index = 0;
+                for(d = readdir(dir); d != NULL; d = readdir(dir)) {
+                    names[index] = from + d->d_name;
+                    index++;
+                }
+                closedir(directory);
+            } else {
+                cerr << "Error detected!" << endl;
+            }
             array<string, sizeof(arrayLenght)> values;
             array<string, sizeof(arrayLenght)> parents;
             values[0] = from;
             parents[0] = "";
-            array<string, 0> files; // = Directory.GetFiles(from.FullName, "*", SearchOption.AllDirectories);
-            array<string, 0> directories; //= Directory.GetDirectories(from.FullName, "*", SearchOption.AllDirectories);
-            array<string, 0> currentValues;// = new string[files.Length + directories.Length];
-            files.CopyTo(currentValues, 0);
-            directories.CopyTo(currentValues, files.Length);
-            for(int i = 1; i <= currentValues.Length; i++) {
-                values[i] = currentValues[i];
-                parents[i] = Directory.GetParent(currentValues[i]).FullName;
+            for(int i = 1; i <= arrayLenght; i++) {
+                values[i] = names[i];
+                string parent = "";
+                int k = 0;
+                int len = lenght(names[i]);
+                if(names[i][len - 1] != '/') {
+                    for(int j = len; j > 0; j--) {
+                        k++;
+                        if(names[i][j] == '/') {
+                            break;
+                        }
+                    }
+                    for(int j = 0; j < j < len - k; j++) {
+                        parent += names[i][j];
+                    }
+                }
+                parents[i] = parent;
             }
-            tree = new Tree(values, parents);
+            //tree = new Tree(values, parents);
         }
         void startChonk() {
             
---
+++ C++ [beta]/tree.h
@@ -0,0 +1,18 @@
+#include <array>
+#include <string>
+using namespace std;
+class tree {
+    public:
+        //treeNode* nodes;
+        int lenght;
+        tree(string* values, string* parents, int len) {
+            lenght = len;
+            int i = 0;
+            /*
+            foreach(object value in values) {
+                nodes[i] = new TreeNode(value, parents[i]);
+                i++;
+            }
+            */
+        }
+};
